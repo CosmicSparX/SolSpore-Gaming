52eb4ae72b1a4a02b4d32c7580fca196e5ee0166
CosmicSparX <kpranay661@gmail.com>
2026-01-12 13:14:49 +0530

    Multilipi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,17 +19,23 @@ export default function RootLayout({
         <link rel="icon" href="/favicons/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
         <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link href="https://sol-spore.vercel.app/" hreflang="x-default" rel="alternate" />
+
+        <link href="https://hi.sol-spore.vercel.app/" hreflang="hi" rel="alternate" />
+
+        <script src="https://script-cdn.multilipi.com/static/JS/page_translations.js" multilipi-key="23eb36b7-b662-4a8b-82be-9b11f27674e8" mode="auto" data-pos-x="50" data-pos-y="50" crossorigin="anonymous" defer>
+        </script>
       </head>
       <body className={`${inter.className} min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800`}>
         <AuthProvider>
           <WalletContextProvider>
-        <div className="min-h-screen flex flex-col">
-          {/* Navbar will be imported in each page */}
+            <div className="min-h-screen flex flex-col">
+              {/* Navbar will be imported in each page */}
               <main className="flex-grow pt-16">
-            {children}
-          </main>
-          {/* Footer will be imported in each page */}
-        </div>
+                {children}
+              </main>
+              {/* Footer will be imported in each page */}
+            </div>
             <Toaster position="bottom-right" />
           </WalletContextProvider>
         </AuthProvider>
